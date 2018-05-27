804d9031f422df878dc6307feaa8a95116a8cb22
GetUrApp <mail@geturapp.com>
2018-05-27 21:35:59 +0530

    Apply styles

--- src/providers/congnito/congnito.ts
+++ src/providers/congnito/congnito.ts
@@ -135,6 +135,22 @@ export class CongnitoProvider {
     });
   }
 
+  forgotPassword() {
+    this.cognitoUser.forgotPassword({
+      onSuccess: function(result) {
+        console.log('call result: ' + result);
+      },
+      onFailure: function(err) {
+        alert(err);
+      },
+      inputVerificationCode() {
+        var verificationCode = prompt('Please input verification code ', '');
+        var newPassword = prompt('Enter new password ', '');
+        this.cognitoUser.confirmPassword(verificationCode, newPassword, this);
+      }
+    });
+  }
+
   getUserPool(username) {
     const userData = {
       Username: username,
