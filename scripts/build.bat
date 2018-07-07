f1a62c707decefead12a0daecb37a2f7ef227d86
jsettypalli <jsettypalli@outlook.com>
2018-07-07 20:59:14 +0530

    - modified build script and created convenience script to build oliver mobile app

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,22 +1,24 @@
 
-set mode=%1
-set client=%2%
-
-REM mode should be either mobile or web
-if NOT %mode% == mobile  if NOT %mode% == web (
-   echo "Please set valid mode to build the app. Acceptable modes are  mobile or web".
-   echo "usage: build.bat <mode> <client>"
-   echo "mode can be either mobile or web"
-   echo "client can be either oliver or busbee"
-   exit /B
-)
+set client=%1
+set mode=%2
 
 REM client should be either oliver or busbee
 if NOT %client% == oliver  IF NOT %client% == busbee (
    echo "Please set valid client to build the app. Acceptable clients are  oliver or busbee".
-   echo "usage: build.bat <mode> <client>"
-   echo "mode can be either mobile or web"
+   echo "usage: build.bat <client> <mode>"
    echo "client can be either oliver or busbee"
+   echo "mode can be either mobile or web"
+
+   exit /B
+)
+
+REM mode should be either mobile or web
+if NOT %mode% == mobile  if NOT %mode% == web (
+   echo "Please set valid mode to build the app. Acceptable modes are  mobile or web".
+   echo "usage: build.bat <client> <mode>"
+   echo "client can be either oliver or busbee"
+   echo "mode can be either mobile or web"
+
    exit /B
 )
 
